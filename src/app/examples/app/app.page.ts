6167a118d13d5ea65f16d9cd65ab6c32a222af75
AZBZ34 <alexbeazley18@outlook.com>
2021-10-23 08:30:12 +1100

    app state change working through web client.

--- src/app/examples/app/app.page.ts
+++ src/app/examples/app/app.page.ts
@@ -1,4 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { App } from '@capacitor/app';
+
+App.addListener('appStateChange', ({ isActive }) => {
+  console.log('App state changed. Is active?', isActive);
+});
+
+App.addListener('appUrlOpen', data => {
+  console.log('App opened with URL:', data);
+});
+
+App.addListener('appRestoredResult', data => {
+  console.log('Restored state:', data);
+});
+
+const checkAppLaunchUrl = async () => {
+  const { url } = await App.getLaunchUrl();
+
+  alert('App opened with URL: ' + url);
+};
 
 @Component({
   selector: 'app-app',
@@ -10,6 +29,10 @@ export class AppPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  appChangeTest() {
+    window.open('https://capacitorjs.com/docs/apis', '_blank');
   }
 
 }
