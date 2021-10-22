ef40b870039ce0619a2700ca4268dadb01959ede
AZBZ34 <alexbeazley18@outlook.com>
2021-10-23 10:02:36 +1100

    camera working on mobile.

--- src/app/examples/camera/camera.page.ts
+++ src/app/examples/camera/camera.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
   selector: 'app-camera',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./camera.page.scss'],
 })
 export class CameraPage implements OnInit {
+  imageUrl: string;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  async testCamera() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    });
+
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    this.imageUrl = image.webPath;
+
+    // Can be set to the src of an image now
+    //imageElement.src = imageUrl;
+  };
 
 }
