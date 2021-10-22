462c299def8d6e9f1c846f424376a162d8ef3ea2
AZBZ34 <alexbeazley18@outlook.com>
2021-10-22 22:42:40 +1100

    action-sheet example working on mobile.

--- src/app/examples/action-sheet/action-sheet.page.ts
+++ src/app/examples/action-sheet/action-sheet.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheet, ActionSheetButtonStyle } from '@capacitor/action-sheet';
+
 
 @Component({
   selector: 'app-action-sheet',
   templateUrl: './action-sheet.page.html',
   styleUrls: ['./action-sheet.page.scss'],
 })
+
 export class ActionSheetPage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  async actionSheetTest() {
+    const result = await ActionSheet.showActions({
+      title: 'Photo Options',
+      message: 'Select an option to perform',
+      options: [
+        {
+          title: 'Upload',
+        },
+        {
+          title: 'Share',
+        },
+        {
+          title: 'Remove',
+          style: ActionSheetButtonStyle.Destructive,
+        },
+      ],
+    });
+    console.log('Action Sheet result:', result);
   }
 
 }
