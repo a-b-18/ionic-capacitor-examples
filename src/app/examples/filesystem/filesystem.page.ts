a6700873881646ae4efdca35af6a504a3936a909
AZBZ34 <alexbeazley18@outlook.com>
2021-10-23 12:12:16 +1100

    filesystem working on web client.

--- src/app/examples/filesystem/filesystem.page.ts
+++ src/app/examples/filesystem/filesystem.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 
 @Component({
   selector: 'app-filesystem',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilesystemPage implements OnInit {
 
+  filePath = 'insert file path here.';
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  async testWriteSecretFile() {
+    await Filesystem.writeFile({
+      path: 'secrets/text.txt',
+      data: "This is a test",
+      directory: Directory.Documents,
+      encoding: Encoding.UTF8,
+    });
+  };
+
+  async testReadSecretFile() {
+    const contents = await Filesystem.readFile({
+      path: 'secrets/text.txt',
+      directory: Directory.Documents,
+      encoding: Encoding.UTF8,
+    });
+
+    console.log('secrets:', contents);
+  };
+
+  async testDeleteSecretFile() {
+    await Filesystem.deleteFile({
+      path: 'secrets/text.txt',
+      directory: Directory.Documents,
+    });
+  };
+
+  async testReadFilePath() {
+    // Here's an example of reading a file with a full file path. Use this to
+    // read binary data (base64 encoded) from plugins that return File URIs, such as
+    // the Camera.
+
+    console.log('Reading from:', this.filePath);
+    const contents = await Filesystem.readFile({
+      path: this.filePath
+    });
+
+    console.log('data:', contents);
+  };
 
 }
