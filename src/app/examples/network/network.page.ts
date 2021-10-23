232684206085fd62fd13c8231d8298e13ebefa13
AZBZ34 <alexbeazley18@outlook.com>
2021-10-23 18:59:13 +1100

    screen-reader works on mobile. (requires talk-back enabled)

--- src/app/examples/network/network.page.ts
+++ src/app/examples/network/network.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Network } from '@capacitor/network';
 
 @Component({
   selector: 'app-network',
@@ -10,6 +11,18 @@ export class NetworkPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  testNetwork() {
+    Network.addListener('networkStatusChange', status => {
+      console.log('Network status changed', status);
+    });
+
+    const logCurrentNetworkStatus = async () => {
+      const status = await Network.getStatus();
+
+      console.log('Network status:', status);
+    };
   }
 
 }
--- src/app/examples/screen-reader/screen-reader.page.ts
+++ src/app/examples/screen-reader/screen-reader.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ScreenReader } from '@capacitor/screen-reader';
 
 @Component({
   selector: 'app-screen-reader',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScreenReaderPage implements OnInit {
 
+  stateChange: any = 'screenReaderStateChange';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  testScreenReader() {
+    ScreenReader.addListener(this.stateChange, ({ value }) => {
+      console.log(`Screen reader is now ${value ? 'on' : 'off'}`);
+    });
+  }
+
+  async checkScreenReader() {
+    const { value } = await ScreenReader.isEnabled();
+
+    alert('Voice over enabled? ' + value);
+  };
+
+  async testSpeech() {
+    await ScreenReader.speak({ value: 'Hello World!' });
+  };
 }
