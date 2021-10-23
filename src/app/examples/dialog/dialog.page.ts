eb5162edc7a9d559fd5567efa6bbc4c269b1ffaf
AZBZ34 <alexbeazley18@outlook.com>
2021-10-23 11:30:20 +1100

    dialog working on web client.

--- src/app/examples/dialog/dialog.page.ts
+++ src/app/examples/dialog/dialog.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Dialog } from '@capacitor/dialog';
 
 @Component({
   selector: 'app-dialog',
@@ -11,5 +12,31 @@ export class DialogPage implements OnInit {
 
   ngOnInit() {
   }
+
+  async testAlert() {
+    await Dialog.alert({
+      title: 'Stop',
+      message: 'this is an error',
+    });
+  };
+
+  async testConfirm() {
+    const { value } = await Dialog.confirm({
+      title: 'Confirm',
+      message: `Are you sure you'd like to press the red button?`,
+    });
+
+    console.log('Confirmed:', value);
+  };
+
+  async testPrompt() {
+    const { value, cancelled } = await Dialog.prompt({
+      title: 'Hello',
+      message: `What's your name?`,
+    });
+
+    console.log('Name:', value);
+    console.log('Cancelled:', cancelled);
+  };
 
 }
