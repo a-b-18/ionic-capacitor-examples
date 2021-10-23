e940387bd0b67654fc5ba28c7d117406c0d1ac03
AZBZ34 <alexbeazley18@outlook.com>
2021-10-23 11:17:28 +1100

    clipboard works on web client.

--- src/app/examples/clipboard/clipboard.page.ts
+++ src/app/examples/clipboard/clipboard.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Clipboard } from '@capacitor/clipboard';
 
 @Component({
   selector: 'app-clipboard',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clipboard.page.scss'],
 })
 export class ClipboardPage implements OnInit {
+  clipboardWrite = 'insert text here';
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  async testClipboardWrite() {
+    console.log(this.clipboardWrite);
+    await Clipboard.write({
+      string: this.clipboardWrite
+    });
+  };
+
+  async testClipboardRead() {
+    const { type, value } = await Clipboard.read();
+
+    alert(`Got ${type} from clipboard: ${value}`);
+  };
 
 }
